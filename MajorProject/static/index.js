9f9062630f2219cd7bcf1a816fd2fd2b5c887514
ritikverma2000 <varmaritik04@gmail.com>
2022-05-02 10:05:41 +0530

    updated static and templates folder

---
+++ MajorProject/static/index.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+// }
+
+// function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+// }
